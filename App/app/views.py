cd3d7c5d0cc29b77fa338d80916d705f4e67b6b7
Melissa Kantor <meldkantor@gmail.com>
2015-12-13 00:28:44 -0500

    rendered about.html in views.py

--- App/app/views.py
+++ App/app/views.py
@@ -18,6 +18,11 @@ def index():
     
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+    
+    return render_template('about.html')
+
 @app.route("/library")
 def library():
     entries = None;
